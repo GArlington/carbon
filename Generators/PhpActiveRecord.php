573b733f756b3173a38c5e1905b309c06b72c3ad
Alain Bacon <syntek67@yahoo.com>
2011-11-13 15:14:33 -0500

    Added PhpActiveRecord generator

--- Generators/PhpActiveRecord.php
+++ Generators/PhpActiveRecord.php
@@ -11,31 +11,73 @@ class PhpActiveReport implements IGenerator
 	{
 		foreach($model->manifest as $object) {
 			$writer = new QFileWriter("$dir/$object->name.php");
+			$view = new QTemplate("Generators/views/PhpActiveRecord/".get_class($object).".php");
 			
-			if( $object instanceof Enumeration )
-				$content = $this->RenderEnumeration($object, $writer);
-				
+			if( $object instanceof Entity )
+				$assoc = $this->GetEntityDefaults($object);
+			else
+				$assoc = $this->GetEnumerationValues($object);
+			$viewdata = array(
+				'object' => $object,
+				'namespace' => $model->namespace,
+				'license' => $model->license,
+				'assoc' => $assoc
+			);
+			if( $object instanceof Entity )
+				$viewdata['includes'] = $this->GetRequiredFilenames($object);
+			
+			$content = $view->Load($viewdata);
+			$writer->Write("<?php\n$content");	
 			$writer->Close();
 		}
+	}
+	
+	/**
+	*  Returns entity's required files (for complex types properties).
+	*/
+	private function GetRequiredFilenames($entity)
+	{
+		$result = array();
+		foreach( $entity->properties as $property )
+			if( !in_array($property->type, Model::$BASE_TYPES) && !in_array($property->type, $result) )
+				$result[] = $property->type;
+		return $result;
+	}
+	
+	/**
+	*  Returns entity's default values indexed by property name.
+	*/
+	private function GetEntityDefaults($entity)
+	{
+		$result = array();
+		foreach( $entity->properties as $property ) {
+			$default = $property->default;
+			if( $property->type == 'text' || $property->type == 'time' )
+				$default = "'".str_replace("'", "\\'",$default)."'";
+			elseif($property->type == 'integer')
+				$default = (int)$default;
+			elseif($property->type == 'real')
+				$default = (double)$default;
+			elseif($property->type == 'logical')
+				$default = $default=='true' ? 'true' : 'false';	
+			elseif($property->type == 'binary')
+				$default = "''";
+			else
+				$default = "0";			
+			$result[$property->name] = $default;
+		}
+		return $result;
 	}	
 	
-	private function RenderEnumeration($enum, $writer)
+	/**
+	*  Returns enumeration values' effective value (not name)
+	*/
+	private function GetEnumerationValues($enum)
 	{
-		$writer->Writeln("<?php");
-		$writer->Writeln();
-		if( $enum->comment ) {
-			$writer->Writeln("/**");
-			$writer->Writeln("*  $enum->comment");
-			$writer->Writeln("*/");
-		}
-		$writer->Writeln("class $enum->name");
-		$writer->Writeln("{");
-		$padding = $maxlen = max(array_map('strlen', array_keys($enum->values)));
-		foreach($enum->values as $value) {
-			$s = sprintf("\tpublic const %s = %s %s", str_pad($value->name,$padding), str_pad("'$value->value';",$padding+4), $value->comment ? "// $value->comment" : '');
-			$writer->Writeln($s);
-		}
-		$writer->Writeln("}");
+		$result = array();
+		foreach( $enum->values as $value )
+			$result[$value->name] = $value->value;
+		return $result;
 	}
 	
 	
