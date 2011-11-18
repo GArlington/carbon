c60c2d00f4b39d7547676b2d892ea0340ea8a33c
Alain Bacon <syntek67@yahoo.com>
2011-11-18 12:49:41 -0500

    updated some invalid templates paths

--- Generators/DataDictionary.php
+++ Generators/DataDictionary.php
@@ -12,11 +12,11 @@ class DataDictionary implements IGenerator
 		@mkdir("$dir/lib");
 		@mkdir("$dir/css");
 
-		copy("Generators/views/lib/jquery-1.6.4.min.js", "$dir/lib/jquery-1.6.4.min.js");
-		copy("Generators/views/lib/LICENSE", "$dir/lib/LICENSE");
-		copy("Generators/views/lib/springy.js", "$dir/lib/springy.js");
-		copy("Generators/views/lib/springyui.js", "$dir/lib/springyui.js");
-		copy("Generators/views/css/style.css", "$dir/css/style.css");
+		copy("Generators/Templates/lib/jquery-1.6.4.min.js", "$dir/lib/jquery-1.6.4.min.js");
+		copy("Generators/Templates/lib/LICENSE", "$dir/lib/LICENSE");
+		copy("Generators/Templates/lib/springy.js", "$dir/lib/springy.js");
+		copy("Generators/Templates/lib/springyui.js", "$dir/lib/springyui.js");
+		copy("Generators/Templates/css/style.css", "$dir/css/style.css");
 
 		// Generate individual pages...
 		foreach($model->manifest as $object) {
@@ -24,7 +24,7 @@ class DataDictionary implements IGenerator
 				continue;
 
 			$writer = new QFileWriter("$dir/$object->name.html");
-			$view = new QTemplate("Generators/views/DataDictionary/".strtolower(get_class($object)).".php");
+			$view = new QTemplate("Generators/Templates/DataDictionary/".strtolower(get_class($object)).".php");
 			$data = array(
 				'object' => $object,
 				'namespace' => $model->namespace,
@@ -38,7 +38,7 @@ class DataDictionary implements IGenerator
 
 		// Generate index page...
 		$writer = new QFileWriter("$dir/index.html");
-		$view = new QTemplate("Generators/views/DataDictionary/index.php");
+		$view = new QTemplate("Generators/Templates/DataDictionary/index.php");
 		$data = array(
 			'index' => $model->packages,
 			'namespace' => $model->namespace,
