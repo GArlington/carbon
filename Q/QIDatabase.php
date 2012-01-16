1d5fff451f6a5eb6ff63159454ddd1fb93db6eaf
Alain Bacon <alain.bacon@emergensys.net>
2012-01-16 12:36:32 -0500

    renamed Q database interface

--- Q/QIDatabase.php
+++ Q/QIDatabase.php
@@ -4,18 +4,18 @@
 /**
 *  Q database interface.
 */
-interface QIDatabase
+interface IQDatabase
 {
 	function Connect($connstr);
 	function Close();
-	
+
 	function SetAutoCommit($value);
-	function Commit();	
+	function Commit();
 	function Rollback();
-	
+
 	function Query($sql);
 	function Execute($sql);
-	
+
 	function LastID();
 	function LastError();
 }
